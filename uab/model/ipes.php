66cba08cba871cfc059ed3568c9dc9236e32942a
Gabriel Fedel <fedel@fedel.net.br>
2016-02-02 23:34:34 -0200

    Pequena correção

--- uab/model/ipes.php
+++ uab/model/ipes.php
@@ -12,8 +12,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
-
 $sql = "SELECT sigla, lat, log FROM ipes";
 $result = $conn->query($sql);
 $rows = array();
@@ -21,7 +19,7 @@ $rows = array();
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc())
     $rows[]=$row;
-} 
+}
 
 print json_encode($rows);
 
