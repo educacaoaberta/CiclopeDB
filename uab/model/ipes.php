8389ae38db4407a98aece7f0eeaacddfcbd248c8
Gabriel Fedel <fedel@fedel.net.br>
2016-02-02 23:37:30 -0200

    Corrigindo select

--- uab/model/ipes.php
+++ uab/model/ipes.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-$sql = "SELECT sigla, lat, log FROM ipes";
+$sql = "SELECT `sigla`, `lat`, `long` FROM ipes";
 $result = $conn->query($sql);
 $rows = array();
 
