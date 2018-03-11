55793217b4d733a6519770b83f2f4837fd8408cd
Cícero Neto <cicero.alex@gmail.com>
2018-03-11 15:30:07 -0300

    Adicionado o DataTables com os dados dos polos e IPES

--- src/components/Dados.vue
+++ src/components/Dados.vue
@@ -1,17 +1,38 @@
 <template>
-  <div>
-
-    <h1>Dados</h1>
-
-<table id="allunits" class="display" cellspacing="0" width="100%">
-    <thead>
-    <tr>
-        <th>Tipo</th>
-        <th>Nome</th>
-        <th>Cidade</th>
-        <th>UF</th>
-    </tr>
-    </thead>
-</table>
-  </div>
+    <div>
+        <h1>Dados</h1>
+        <table id="allunits" class="display" cellspacing="0" width="100%">
+            <thead>
+            <tr>
+                <th>Tipo</th>
+                <th>Nome</th>
+                <th>Cidade</th>
+                <th>UF</th>
+            </tr>
+            </thead>
+        </table>
+    </div>
 </template>
+
+<script>
+  export default {
+    mounted () {
+      $('#allunits').DataTable({
+        "ajax": {
+          "url": "/static/json/lista-unidades.geojson",
+          "dataSrc": "features"
+        },
+        "language": {
+          "url": "/static/json/datatables_pt-br.json"
+        },
+          "columns": [
+          { "data": "properties.tipo" },
+          { "data": "properties.nome" },
+          { "data": "properties.cidade" },
+          { "data": "properties.estado" }
+        ]
+      });
+    }
+  }
+
+</script>
