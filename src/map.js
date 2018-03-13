d892a79474be349f0af7f9e15c32b31168b8f7b0
Cícero Neto <cicero.alex@gmail.com>
2018-03-13 10:37:54 -0300

    Arquivo que carrega o mapa foi refatorado

--- src/map.js
+++ src/map.js
@@ -1,40 +1,118 @@
 import mapboxgl from 'mapbox-gl';
 import { EventBus } from './eventBus';
 
-var accessToken = 'pk.eyJ1IjoibWFwYXVhYiIsImEiOiJjamVpbDZsazgzNWJyMnFxZTN0Z2diczc5In0.x09vnBwbadLIJ-HMq-E8sg';
-var brasilStyle = 'mapbox://styles/mapauab/cjeil7fjv0rxm2ro2jll0x412';
-var ipesLayer = 'mapbox://mapauab.cjeim2fzs0tfi2qmo3w4mqixu-1qo7e';
-var ipesSourceLayer = 'ipes-data';
-var polosLayer = 'mapbox://mapauab.cjeim1kte2inx39o7p1owd9ei-97832';
-var polosSourceLayer = 'polos-data';
-var pinImage = require('./images/pin.png');
+const accessToken = 'pk.eyJ1IjoibWFwYXVhYiIsImEiOiJjamVpbDZsazgzNWJyMnFxZTN0Z2diczc5In0.x09vnBwbadLIJ-HMq-E8sg';
+
+const brasilStyle = 'mapbox://styles/mapauab/cjeil7fjv0rxm2ro2jll0x412';
+
+const ipesHasPolosLayer = 'mapbox://mapauab.cjeoi79nn02l438qy7t1y0p66-6fffu';
+const ipesHasPolosSourceLayer = 'ipes-has-polos';
+
+const ipesLayer = 'mapbox://mapauab.cjeim2fzs0tfi2qmo3w4mqixu-1qo7e';
+const ipesSourceLayer = 'ipes-data';
+
+const polosLayer = 'mapbox://mapauab.cjeim1kte2inx39o7p1owd9ei-97832';
+const polosSourceLayer = 'polos-data';
+
+const pinImage = require('./images/pin.png');
 
 mapboxgl.accessToken = accessToken;
-var map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
   container: 'map',
   style: brasilStyle,
   zoom: 3.5,
   center: [-41.540733, -14.720560],
   // maxZoom: 5,
   // minZoom: 1,
-  trackResize: true
 });
 
-map.resize();
+// cria um novo popup
+let popup = new mapboxgl.Popup({
+  closeButton: false,
+  closeOnClick: false
+});
 
 map.on('load', function() {
   // add navigation control
-  var navControl = new mapboxgl.NavigationControl();
+  let navControl = new mapboxgl.NavigationControl();
   map.addControl(navControl, 'top-left');
 
   // add layers button
-  var layersControl = new LayersControl();
+  const layersControl = new LayersControl();
   map.addControl(layersControl, 'top-left');
 
+  addIpesLayer();
+  addPolosLayer();
+  addIpesHasPolos();
+});
+
+function addIpesLayer() {
+  // carrega os ipes
+  map.addSource("ipes", {
+    type: "vector",
+    url: ipesLayer
+  });
+
+  // carrega o pin e adiciona no layer dos ipes
+  map.loadImage(pinImage, function(error, image) {
+    if (error) throw error;
+    map.addImage('pin', image);
+    map.addLayer({
+      'id': 'ipes',
+      'type': 'symbol',
+      'source': 'ipes',
+      'source-layer': ipesSourceLayer,
+      layout: {
+        "icon-image": "pin",
+        'visibility': 'visible'
+      }
+    });
+  });
+
+  // muda o cursor quando entrar em um ipes
+  // adiciona o popup com a sigla do ipes
+  map.on('mouseenter', 'ipes', function(e) {
+    map.getCanvas().style.cursor = 'pointer';
+
+    let coordinates = e.lngLat;
+    let sigla = e.features[0].properties.sigla;
+
+    popup.setLngLat(coordinates)
+      .setText(sigla.toUpperCase())
+      .addTo(map);
+  });
+
+  // o cursor volta ao padrão ao sair
+  map.on('mouseleave', 'ipes', function() {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+  });
+
+  // ao clicar em um ipes...
+  map.on('click', 'ipes', function(e) {
+    EventBus.$emit('infoIpes', e.features[0].properties);
+
+    if (!e.features.length) return
+    const feature = e.features[0];
+
+    console.log(feature)
+    const sigla = feature.properties.sigla
+    map.setLayoutProperty('centros', 'visibility', 'visible');
+    map.setFilter('centros', [
+      'all',
+      ['==', 'sigla', sigla]
+    ]);
+
+    EventBus.$emit('switchInfoPolos', false);
+    EventBus.$emit('switchInfoIpes', true);
+  });
+}
+
+function addPolosLayer() {
   // carrega os polos
   map.addSource("polos", {
     type: 'vector',
-    url: polosLayer
+    url: polosLayer,
   });
 
   // adiciona os círculos nos polos
@@ -59,35 +137,13 @@ map.on('load', function() {
           [22, 17]
         ]
       }
-    }
+    },
   });
 
-  // carrega os ipes
-  map.addSource("ipes", {
-    type: "vector",
-    url: ipesLayer
-  });
-
-  // carrega o pin e adiciona no layer dos ipes
-  map.loadImage(pinImage, function(error, image) {
-    if (error) throw error;
-    map.addImage('pin', image);
-    map.addLayer({
-      'id': 'ipes',
-      'type': 'symbol',
-      'source': 'ipes',
-      'source-layer': ipesSourceLayer,
-      layout: {
-        "icon-image": "pin",
-        'visibility': 'visible'
-      }
-    });
-  });
-
-  // cria um novo popup
-  var popup = new mapboxgl.Popup({
-    closeButton: false,
-    closeOnClick: false
+  // o cursor volta ao padrão ao sair
+  map.on('mouseleave', 'polos', function() {
+    map.getCanvas().style.cursor = '';
+    popup.remove();
   });
 
   // muda o cursor para pointer quando entrar em um polo
@@ -95,19 +151,13 @@ map.on('load', function() {
   map.on('mouseenter', 'polos', function(e) {
     map.getCanvas().style.cursor = 'pointer';
 
-    var coordinates = e.lngLat;
-    var nome_fantasia = e.features[0].properties.nome_fantasia;
+    let coordinates = e.lngLat;
+    let nome_fantasia = e.features[0].properties.nome_fantasia;
 
     // configura o popup para mostrar o nome do polo
     popup.setLngLat(coordinates)
       .setText(nome_fantasia.toUpperCase())
       .addTo(map);
-  });
-
-  // o cursor volta ao padrão ao sair
-  map.on('mouseleave', 'polos', function() {
-    map.getCanvas().style.cursor = '';
-    popup.remove();
   });
 
   // ao clicar em um polo...
@@ -117,43 +167,64 @@ map.on('load', function() {
     EventBus.$emit('switchInfoIpes', false);
     EventBus.$emit('switchInfoPolos', true);
   });
+}
 
-  // muda o cursor quando entrar em um ipes
-  // adiciona o popup com a sigla do ipes
-  map.on('mouseenter', 'ipes', function(e) {
-    map.getCanvas().style.cursor = 'pointer';
-
-    var coordinates = e.lngLat;
-    var sigla = e.features[0].properties.sigla;
-
-    popup.setLngLat(coordinates)
-      .setText(sigla.toUpperCase())
-      .addTo(map);
-
+function addIpesHasPolos() {
+  // carrega os all ipes (ipes_has_polos)
+  map.addSource("ipes-has-polos", {
+    type: "vector",
+    url: ipesHasPolosLayer
   });
 
-  // ao clicar em um ipes...
-  map.on('click', 'ipes', function(e) {
-    EventBus.$emit('infoIpes', e.features[0].properties);
+  map.addLayer({
+    "id": "centros",
+    "source": "ipes-has-polos",
+    'source-layer': ipesHasPolosSourceLayer,
+    "type": "circle",
+    "paint": {
+      "circle-color": '#4FB0C6',
+      "circle-stroke-width": 2,
+      "circle-stroke-color": '#000',
+      "circle-radius": {
+        "base": 1,
+        "stops": [
+          // zoom is 0 -> radius will be 5px
+          [0, 2],
+          // zoom is 5 -> radius will be 15px
+          [22, 17]
+        ]
+      }
+    },
+    layout: {
+      'visibility': 'none'
+    }
+  });
 
-    EventBus.$emit('switchInfoPolos', false);
-    EventBus.$emit('switchInfoIpes', true);
+  map.on('mouseenter', 'centros', function(e) {
+    map.getCanvas().style.cursor = 'pointer';
 
-    // remove a div e adiciona para que não se repita o gráfico
-    // $('#polosBarChart').remove();
-    // $('#graphContainer').append('<canvas id="polosBarChart"><canvas>');
+    let coordinates = e.lngLat;
+    let nome_polo = e.features[0].properties.nome_polo;
 
-    // var myBarChart = loadChart("polosBarChart", "bar", "Polos");
-    // var sigla = e.features[0].properties.sigla;
-    // processBarChartIpesWithSiglaIpes(myBarChart, sigla);
+    // configura o popup para mostrar o nome do polo
+    popup.setLngLat(coordinates)
+      .setText(nome_polo.toUpperCase())
+      .addTo(map);
   });
 
   // o cursor volta ao padrão ao sair
-  map.on('mouseleave', 'ipes', function() {
+  map.on('mouseleave', 'centros', function() {
     map.getCanvas().style.cursor = '';
     popup.remove();
   });
-});
+
+  map.on('click', 'centros', function(e) {
+    EventBus.$emit('infoPolo', e.features[0].properties);
+
+    EventBus.$emit('switchInfoIpes', false);
+    EventBus.$emit('switchInfoPolos', true);
+  });
+}
 
 class LayersControl {
 
@@ -167,21 +238,19 @@ class LayersControl {
     // adiciona o botão em um container
     this._container = document.createElement('div');
     this._container.className = 'mapboxgl-ctrl mapboxgl-ctrl-group';
-    this._container.appendChild(this._layermenu)
-    this._container.appendChild(this._button)
+    this._container.appendChild(this._layermenu);
+    this._container.appendChild(this._button);
 
     // ao entrar no container
     this._container.addEventListener('mouseenter', function() {
-      let menu = this.firstChild
-      console.log(this)
-      menu.className = ''
+      let menu = this.firstChild;
+      menu.className = '';
 
       menu.addEventListener('click', function() {
-        let input = $( "#menu input" );
+        let input = $("#menu").find("input" );
         for (let i = 0; i < input.length; i++) {
           let visibility = map.getLayoutProperty(input[i].id, 'visibility');
 
-          console.log(input[i].id + ": " + visibility)
           if(input[i].checked === true) {
             map.setLayoutProperty(input[i].id, 'visibility', 'visible');
           } else {
