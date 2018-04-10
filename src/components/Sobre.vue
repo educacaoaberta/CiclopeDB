2355aefa23cd29e7c33d7a52256a07f6b231873f
Cícero Neto <cicero.alex@gmail.com>
2018-04-10 09:57:07 -0300

    Pequena alteração no 'Sobre'
    - Adicionado a data da 'Última atualização';
    - Adicionado a classe 'unprose link' para os links;

--- src/components/Sobre.vue
+++ src/components/Sobre.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <section id="sobre">
-    <p>Última atualização: XX-XX-XXXX</p>
+    <p><b>Última atualização:</b> 15/01/2018</p>
       <section id="projeto" class="pb12">
         <div class="txt-l txt-bold">O projeto</div>
         <p>Este mapa apresenta as Instituições de Ensino Superior (IPES) e os polos que fazem parte do Sistema Universidade Aberta do Brasil. Os dados foram fornecidos pelas bases de dados da DED/CAPES.
@@ -11,23 +11,23 @@
       </section>
 
       <section id="introducao" class="pb12">
-          <p>Este é um projeto que foi criado por iniciativa do grupo de pesquisa <a href="http://dgp.cnpq.br/dgp/espelhogrupo/8749855854098226">Formação docente e tecnologias de informação e comunicação</a> que desde 2011 estuda a Universidade Aberta do Brasil (<a href="http://www.uab.capes.gov.br/">UAB</a>).O projeto de mapeamento foi conduzido pela <a href="http://educacaoaberta.org/">Cátedra UNESCO em Educação Aberta</a> na UNICAMP em parceria com a Universidade de Brasília com apoio da Diretoria de Educação a Distância (DED)/CAPES.</p>
+          <p>Este é um projeto que foi criado por iniciativa do grupo de pesquisa <a class="unprose link" href="http://dgp.cnpq.br/dgp/espelhogrupo/8749855854098226">Formação docente e tecnologias de informação e comunicação</a> que desde 2011 estuda a Universidade Aberta do Brasil (<a class="unprose link" href="http://www.uab.capes.gov.br/">UAB</a>).O projeto de mapeamento foi conduzido pela <a class="unprose link" href="http://educacaoaberta.org/">Cátedra UNESCO em Educação Aberta</a> na UNICAMP em parceria com a Universidade de Brasília com apoio da Diretoria de Educação a Distância (DED)/CAPES.</p>
       </section>
 
       <section id="software" class="pb12">
         <div class="txt-l txt-bold">O software</div>
-        <p>Caso identifique algum problema, dado incongruente, ou queira ajudar a melhorar o sistema, entre em contato colocando um <i>problema/comentário</i> na nossa página no <a href="http://github.com/educacaoaberta/CiclopeDB/issues">Github</a>.</p>
+        <p>Caso identifique algum problema, dado incongruente, ou queira ajudar a melhorar o sistema, entre em contato colocando um <i>problema/comentário</i> na nossa página no <a class="unprose link" href="http://github.com/educacaoaberta/CiclopeDB/issues">Github</a>.</p>
       </section>
 
       <section id="pesquisadores" class="pb12">
-        <p>Tel Amiel (<a href="http://fe.unb.br">FE/UnB</a>) - Coordenador do projeto | <a href="https://github.com/tamielbr">/tamielbr</a></p>
-        <p>Cícero Neto - Desenvolvedor backend e frontend | <a href="http://github.com/greevin/">/greevin</a></p>
-        <p>Gabriel Fedel - <a href="http://www.lnls.cnpem.br">LNLS</a> | Desenvolvedor backend e consultor <a href="https://github.com/gabrielfedel">/gabrielfedel</a></p>
-        <p>Kalimar Maia (<a href="http://mapbox.com">Mapbox</a>) - Consultor técnico <a href="https://github.com/kalimar">/kalimar</a></p>
+        <p>Tel Amiel (<a class="unprose link" href="http://fe.unb.br">FE/UnB</a>) - Coordenador do projeto | <a class="unprose link" href="https://github.com/tamielbr">/tamielbr</a></p>
+        <p>Cícero Neto - Desenvolvedor backend e frontend | <a class="unprose link" href="https://github.com/greevin/">/greevin</a></p>
+        <p>Gabriel Fedel - <a class="unprose link" href="http://www.lnls.cnpem.br">LNLS</a> | Desenvolvedor backend e consultor <a class="unprose link" href="https://github.com/gabrielfedel">/gabrielfedel</a></p>
+        <p>Kalimar Maia (<a class="unprose link" href="http://mapbox.com">Mapbox</a>) - Consultor técnico <a class="unprose link" href="https://github.com/kalimar">/kalimar</a></p>
       </section>
 
       <section id="problemas" class="pb12">
-        <p>O sistema que desenvolvemos, apelidado de <b>Ciclope</b>, é um software livre com licença<a href="https://opensource.org/licenses/GPL-3.0">GPL v3.0</a>. O código está disponível no <a href="http://github.com/educacaoaberta/CiclopeDB">Github</a>.
+        <p>O sistema que desenvolvemos, apelidado de <b>Ciclope</b>, é um software livre com licença<a class="unprose link" href="https://opensource.org/licenses/GPL-3.0">GPL v3.0</a>. O código está disponível no <a class="unprose link" href="http://github.com/educacaoaberta/CiclopeDB">Github</a>.
         </p>
       </section>
 
@@ -37,10 +37,10 @@
         <div class="pb12">
           Para saber um pouco mais sobre a UAB, apontamos trabalhos do nosso grupo de pesquisa
         </div>
-          <p class="txt-s">Amiel, T., Duran, M. R. da C., & Costa, C. J. da. (2017). <a href="https://doi.org/10.17398/1695-288X.16.2.161">Construindo Políticas de Abertura a partir dos Recursos Educacionais Abertos: Uma Análise do Sistema Universidade Aberta do Brasil</a>. Revista Latinoamericana de Tecnología Educativa.</p>
+          <p class="txt-s">Amiel, T., Duran, M. R. da C., & Costa, C. J. da. (2017). <a class="unprose link" href="https://doi.org/10.17398/1695-288X.16.2.161">Construindo Políticas de Abertura a partir dos Recursos Educacionais Abertos: Uma Análise do Sistema Universidade Aberta do Brasil</a>. Revista Latinoamericana de Tecnología Educativa.</p>
           <p class="txt-s">Costa, C. J. da, & Duran, M. R. C. (2011). href="href="http://ojs.rbpg.capes.gov.br/index.php/rbpg/article/view/289/0">http://ojs.rbpg.capes.gov.br/index.php/rbpg/article/view/289/0">A Política Nacional de Formação de Professores entre 2005 e 2010: a nova Capes e o Sistema Universidade Aberta do Brasil</a>. Revista Brasileira de Pós-Graduação.</p>
-          <p class="txt-s">Soares, T. & Amiel .T (2017). <a href="http://educacaoaberta.org/perspectivas-rea-uab/">Recursos educacionais na Universidade Aberta do Brasil: Perspectivas e práticas</a></p>
-          <p class="txt-s">Duran, M. R. da C., Costa, C. J. da, Massaro, T., & Amiel, T. (2012). <a href="http://sistemas3.sead.ufscar.br/ojs/Trabalhos/277-937-3-ED.pdf">http://sistemas3.sead.ufscar.br/ojs/Trabalhos/277-937-3-ED.pdf">Os polos do sistema UAB e seus coordenadores nas regiões nordeste, norte e sul do Brasil</a>. Paper presented at the Simpósio Internacional de Educação a Distância, UFScar.</p>
+          <p class="txt-s">Soares, T. & Amiel .T (2017). <a class="unprose link" href="http://educacaoaberta.org/perspectivas-rea-uab/">Recursos educacionais na Universidade Aberta do Brasil: Perspectivas e práticas</a></p>
+          <p class="txt-s">Duran, M. R. da C., Costa, C. J. da, Massaro, T., & Amiel, T. (2012). <a class="unprose link" href="http://sistemas3.sead.ufscar.br/ojs/Trabalhos/277-937-3-ED.pdf">http://sistemas3.sead.ufscar.br/ojs/Trabalhos/277-937-3-ED.pdf">Os polos do sistema UAB e seus coordenadores nas regiões nordeste, norte e sul do Brasil</a>. Paper presented at the Simpósio Internacional de Educação a Distância, UFScar.</p>
       </section>
 
     </section>
