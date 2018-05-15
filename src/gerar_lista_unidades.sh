72317b7fdbfa61733657f0835567f643d42a071f
Cícero Neto <cicero.alex@gmail.com>
2018-05-14 22:02:58 -0300

    Script para gerar os JSONs atualizados com os dados dos arquivos

---
+++ src/gerar_lista_unidades.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+## brew install zorba
+
+if [ ! -f /usr/local/bin/zorba ]
+then
+    echo "Instalar o zorba (www.zorba.io). No Mac, instalar com o HomeBrew, usando a linha abaixo:"
+    echo "brew install zorba"
+else
+    echo "Gerando um novo arquivo lista-unidades.json"
+    zorba --uri-path '../static/json/' lista_unidades.jq -o ../static/json/lista-unidades.json
+
+    echo "Gerando um novo arquivo polosbystate.json"
+    zorba --uri-path '../static/json/' polosbystate.jq -o ../static/json/polosbystate.json
+
+    echo "Gerando um novo arquivo ipesbystate.json"
+    zorba --uri-path '../static/json/' ipesbystate.jq -o ../static/json/ipesbystate.json
+
+fi
